82f94a2910e6de6a9fe1db13fbbabc0ecc7abe0c
cantstumpthetrump2000 <camerondrain@gmail.com>
2018-10-21 02:45:20 +0100

    hight update

--- Anycubicplus/Configuration.h
+++ Anycubicplus/Configuration.h
@@ -995,7 +995,7 @@ M666 X-0.39 Y0.00 Z-3.89
 // For DELTA this is the top-center of the Cartesian print volume.
 //#define MANUAL_X_HOME_POS 0
 //#define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS 288.7 // Distance between the nozzle to printbed after homing
+#define MANUAL_Z_HOME_POS 288.8 // Distance between the nozzle to printbed after homing
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
 //
